7ca98a15f42ed0f883641d01eee4067c6de1aeb7
ErikeRodrigues <137097229+ErikeRodrigues@users.noreply.github.com>
2023-07-31 11:25:08 -0300

    Inicio do curso de JavaScript

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,12 @@
+//console.log é usado para exibir algo na tela do computador
+console.log('Passo #1')
+console.log('Passo #2')//mais um exemplo de console.log!
+//console.log('Passo #3') o comentario serve para você tb tirar uma linha de codigo que voce nao queira usar igual nesse exemplo
+
+/*
+ * Esse é um 
+ * comentário
+ * de múltiplas
+ * linhas!
+*/
+console.log('Passo #3')
