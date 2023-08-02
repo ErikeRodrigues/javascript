a4d95457a6d83a36f57df5a6664d058b9b9b2edb
ErikeRodrigues <137097229+ErikeRodrigues@users.noreply.github.com>
2023-08-02 19:33:18 -0300

    primeira seção terminada

---
+++ fundamentos/inicio/desafio-area.js
@@ -0,0 +1,9 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592
+const raio = 10
+let areaCalculada = PI * raio * raio
+
+console.log('O valor da área é ' + areaCalculada + ' m2.')
+//console.log(Math.PI)
